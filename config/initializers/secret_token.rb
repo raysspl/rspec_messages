f6c595b1c384047962f9a82c40e37d05317c08f7
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-10-16 10:46:49 -0700

    Initial RSpec commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messages::Application.config.secret_key_base = '070911b70b1af156b905c62facad686914c65d086a54e934cb5405d5bc38599489685bd4f501582b719f62c8ddedce43772957a4257ef7caf9e079ef214de7e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Messages::Application.config.session_store :cookie_store, key: '_messages_session'
